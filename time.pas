c70e03d0f81974400c76d8752356848bd7cf0018
Alexander Nosachev <nosachev2512@gmail.com>
2024-10-05 07:36:13 +0300

    2 task is done

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год: ');
+  if ((year mod 4 = 0) and (not (year mod 100 = 0) and (year mod 400 <> 0))) then
+    Print('Этот год високосный')
+  else
+    Print('Этот год не високосный');
+end.
